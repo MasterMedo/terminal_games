a08516d9c3dc3c25c4012962c6eb38d553a1a1e6
MasterMedo <mislav.vuletic@gmail.com>
2020-08-12 11:24:02 +0200

    hangman: show hyphens in hyphenated compound words

--- hangman.py
+++ hangman.py
@@ -4,6 +4,7 @@ from requests.exceptions import ConnectionError
 from functools import partial
 from itertools import chain
 from random import shuffle
+from string import ascii_lowercase
 
 
 term = Terminal()
@@ -23,15 +24,15 @@ body_parts = [[9, 10, 11],
 
 shuffle(body_parts)
 
-tries, letters = 10, set()
 word = 'hangman'
-
 try:
     response = get('https://random-word.ryanrk.com/api/en/word/random')
     word = response.json()[0].lower()
 except ConnectionError:
     print('You need an internet connection to play this game!\n')
 
+tries = 10
+guessed = set(c for c in word if c not in ascii_lowercase)
 
 with term.cbreak(), term.hidden_cursor():
     while True:
@@ -39,21 +40,21 @@ with term.cbreak(), term.hidden_cursor():
         for i, x in enumerate(man):
             echo(x if i not in chain(*body_parts) else ' ', end='')
 
-        echo('\n' + ' '.join(c if c in letters else '_' for c in word))
+        echo('\n' + ' '.join(c if c in guessed else '_' for c in word))
 
         if tries == 0:
             echo('You lost!')
             break
 
-        if set(word) == letters:
+        if set(word) == guessed:
             echo('Congratulations! You won!')
             break
 
         echo(term.move_up(9))
 
         c = term.inkey()
-        if len(c) == 1 and c in word and c not in letters:
-            letters.add(c)
+        if len(c) == 1 and c in word and c not in guessed:
+            guessed.add(c)
         else:
             tries -= 1
             body_parts = body_parts[:-1]
